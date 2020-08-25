988a72ba542959c88c0ba04cc1c5eb9f21a60765
Antonio <antonioareas@me.com>
2020-08-24 19:50:15 -0700

    First commit

---
+++ Part2/Program.cs
@@ -0,0 +1,32 @@
+﻿/// Homework No. 1		Exercise No. 2
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  August 24, 2020
+///
+/// Problem Statement: Draw something using writeline statements
+/// 
+/// Overall Plan:
+/// 1) use multiple writeline statements to draw something.
+/// 
+
+using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* * * * * * * * * *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("* * * * * * * * * *");
+        }
+    }
+}
